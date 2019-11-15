241c35072fb829a74d07332b92dc0528fcb944d6
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-15 12:28:16 +0300

    1 izmenen deployon20191123

--- deployon20191123.pl
+++ deployon20191123.pl
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Content-type: text/plain\n";
 echo '';
-cd /var/www/domains/g2.qzo.su/ || exit > /dev/null
+cd /var/www/domains/alex.qzo.su/ || exit > /dev/null
 git reset --hard > /dev/null
-git pull ssh://gafurovstudio@bitbucket.org/gafurovstudio/g2.git develop > /dev/null
+git pull https://github.com/mamchura11072000/repozitoriy.git > /dev/null
 #git@bitbucket.org:gafurovstudio/g2.git
