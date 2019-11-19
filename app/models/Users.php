167d54a72e975f17d25f7a69873a8b50ae5a8ca3
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-19 15:05:38 +0300

    25 MyMetod

--- app/models/Users.php
+++ app/models/Users.php
@@ -7,9 +7,9 @@ class Users {
         $result = _MainModel::table("users_cards")->get()->send();}
 
     public function MyMetod(){
-    	$a = $_GET[1];
-    	$b = $_GET[2];
-    	$c = $_GET[3];
+    	$a = $_GET 1;
+    	$b = $_GET 2;
+    	$c = $_GET 3;
     	$summa = $a + $b + $c;
     	echo "Сумма $a + $b + $c = $summa";}
 
