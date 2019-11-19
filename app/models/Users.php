2b39f6f50529ec82053b507d41fac68689156d2c
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-19 15:15:59 +0300

    27 MyMetod

--- app/models/Users.php
+++ app/models/Users.php
@@ -7,8 +7,12 @@ class Users {
         $result = _MainModel::table("users_cards")->get()->send();}
 
     public function MyMetod(){
-    	echo 'Привет, ' . htmlspecialchars($_GET["name"]) . '!';
-    }
+    	
+    	$a = $_GET ['a'];
+    	$b = $_GET ['b'];
+    	$c = $_GET ['c'];
+    	$summa = $a + $b + $c;
+    	echo "Сумма $a + $b + $c = $summa";}
 
          // _MainModel::$string;   
     }
