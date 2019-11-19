ed99e6e9bc82c7beb6a39b725beadac40fecf1f6
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-19 14:36:38 +0300

    17 MyMetod

--- app/models/Users.php
+++ app/models/Users.php
@@ -6,9 +6,9 @@ class Users {
     public function getListUsers(){
         $result = _MainModel::table("users_cards")->get()->send();}
 
-    public function MyMetod(){$string =_MainModel::$_GET["Hellow"];}
+    public function MyMetod(){$string =$_GET["Hellow"];}
 
-          _MainModel::$string;   
+          //_MainModel::$string;   
     }
 
 ?>
