e344578052c2d05e2ba74d330d796c7fa0d93893
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-20 12:34:25 +0300

    1.16 metod

--- app/models/Users.php
+++ app/models/Users.php
@@ -17,7 +17,11 @@ class Users {
     
 
     public function addCardsUser(){
-    	$result1=_MainModel::table("dd_user_cards")->add(array("id"=>"3", "level"=>"1", "user_type"=>"volunter", "image"=>"null", "nickname"=>"sokolov", "rating"=>"6", "description"=>"ghjvhyinj", "role"=>"user", "status"=>"active"))->send();
+    	$result1=_MainModel::table("dd_user_cards")->add(array($a=$_GET['id'], $b=$_GET['level'], $c=$_GET['user_type'], $d=$_GET['image'], $e=$_GET['nickname'], $f=$_GET['rating'], $g=$_GET['description'], $h=$_GET['role'],$i=$_GET['status']))->send();
+
+
+    public function editCardsUser(){
+    	$result1=_MainModel::table("dd_user_cards")->edit(array("id"=>"3", "level"=>"1", "user_type"=>"volunter", "image"=>"null", "nickname"=>"sokolov", "rating"=>"6", "description"=>"ghjvhyinj", "role"=>"user", "status"=>"active"))->send();
 
     	_MainModel::viewJSON($result1); }
 }
--- app/presenters/TestPresenter.php
+++ app/presenters/TestPresenter.php
@@ -11,11 +11,11 @@ class Testpresenter extends MainPresenter {
 
 public function MyMetod(){echo (new Users())->MyMetod();}
 
-public function getListUsers(){ echo (new Users())->getListUsers();
-
-}
+public function getListUsers(){ echo (new Users())->getListUsers();}
 
 public function addCardsUser(){echo (new Users())->addCardsUser();}
+
+public function editCardsUser(){echo (new Users())->editCardsUser();}
 
 	/* labels */
 	//public function ruLableTable(){ $this->renderLabel('rus', 'labelLayoutTable'); }
