06ae6562118842003ffcd64c1866e337f8b6ebd0
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-20 22:19:05 +0300

    addCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -17,7 +17,7 @@ class Users {
     
 
     public function addCardsUser(){
-   		$id = _MainModel::table("dd_user_cards")->add(array( "id"="5", "level" => "3", "user_type" => "volunter", "image" => "null", "nickname" => "dmitriev", "rating" => "5", "description" => "dfghj", "role" => "user", "status" => "active"))->send();
+   		$id = _MainModel::table("dd_user_cards")->add(array( "id"=>"5", "level" => "3", "user_type" => "volunter", "image" => "null", "nickname" => "dmitriev", "rating" => "5", "description" => "dfghj", "role" => "user", "status" => "active"))->send();
 
     	_MainModel::viewJSON($id);
 	}
