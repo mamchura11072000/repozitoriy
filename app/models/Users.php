f229d60f7399b312688cb4902e5df035cab66419
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-20 13:50:38 +0300

    1.30 metod

--- app/models/Users.php
+++ app/models/Users.php
@@ -27,8 +27,8 @@ class Users {
     	$role = $_GET ['h'];
     	$status = $_GET ['h'];
 
-    	$result1=_MainModel::table("dd_user_cards")->add(array($id=>"5", $level=>"3", $user_type=>"volunter", $image=
-    		>"null", $nickname=>"dmitriev", $rating=>"5", $description=>"dfghj", $role=>"user", $status=>"active"))send();
+    	$result1=_MainModel::table("dd_user_cards")->add(array($id="5", $level="3", $user_type="volunter", $image=
+    		"null", $nickname="dmitriev", $rating="5", $description="dfghj", $role="user", $status="active"))send();
 
     _MainModel::viewJSON($result1);}
 
