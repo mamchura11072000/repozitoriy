c1c297b52c351636e41c9466ad53c0bbc2c3ba1d
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-21 22:55:41 +0300

    addCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -18,7 +18,7 @@ class Users {
 
     public function addCardsUser(){
     	
-    	$id = _MainModel::table("dd_user_cards")->add(array( 'id'=>"7", "level" => "3", "user_type" => "volunter", "image" => "null", "nickname" => "dmitriev", "rating" => "5", "description" => "dfghj", "role" => "user", "status" => "active"))->send();
+    	$id = _MainModel::table("dd_user_cards")->add(array( 'id'=>7, "level" => "3", "user_type" => "volunter", "image" => "null", "nickname" => "dmitriev", "rating" => "5", "description" => "dfghj", "role" => "user", "status" => "active"))->send();
     	if(empty("id")){echo "id"; }else{echo 'Ошибка';};
     	if(empty("level")){echo "level"; }else{echo 'Ошибка';};
     	if(empty("user_typ")){echo "user_type";}else{echo 'Ошибка';};
