311b3759a3df4d95f00671886dadbc8c02a3c33d
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-21 22:49:45 +0300

    addCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -23,7 +23,7 @@ class Users {
     	if(isset("level")&&!empty("level")){echo "level"; }else{echo 'Ошибка';};
     	if(isset("user_type")&&!empty("user_typ")){echo "user_type";}else{echo 'Ошибка';};
     	if(isset("image")&&!empty("image")){echo "image"; }else{echo 'Ошибка';};
-    	if(isset("nickname")&&!empty("nickname"))echo "nickname"; }else{echo 'Ошибка';};
+    	if(isset("nickname")&&!empty("nickname")){echo "nickname"; }else{echo 'Ошибка';};
     	if(isset("rating")&&!empty("rating")){echo "rating"; }else{echo 'Ошибка';};
     	if(isset("description")&&!empty("description")){echo "description"; }else{echo 'Ошибка';};
     	if(isset("role")&&!empty("role")){echo "role"; }else{echo 'Ошибка';};
