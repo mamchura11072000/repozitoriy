589cdce61a9c58df4710c135cfc29ba6e3e46cfd
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-22 11:22:21 +0300

    addCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -39,7 +39,7 @@ class Users {
     	$id = _MainModel::table("dd_user_cards")->add(array('level' => $b, 'user_type' => $c, 'image' => $d, 'nickname' => $e, 'rating' => $f, 'description' => $g, 'role' => $h, 'status' => $j))->send();   	
     	 
     	
-    	 	_MainModel::viewJSON($id, $b, $c, $d, $e, $f, $g, $h, $j);
+    	 	_MainModel::viewJSON($id, $err_message);
     	 	
     	 }
 
