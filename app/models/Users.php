2714425c0d734f9088da7000210a2556276027cb
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-22 14:00:25 +0300

    addCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -19,13 +19,13 @@ class Users {
     public function addCardsUser(){
     	
     	if (empty($_GET['level']|| 
-    		'user_type'||
-    		'image'||
-    		'nickname'||
-    		'rating'||
-    		'description'||
-    		'role'||
-    		'status')){
+    		$_GET['user_type']||
+    		$_GET['image']||
+    		$_GET['nickname']||
+    		$_GET['rating']||
+    		$_GET['description']||
+    		$_GET['role']||
+    		$_GET['status']? 'пустая' : 'не пустая';)){
     		echo 'error level';
     	}
     	else{
