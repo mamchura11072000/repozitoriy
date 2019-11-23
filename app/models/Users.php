0743226f4449e8abbab6304b1844a3b869b33640
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-23 16:31:34 +0300

    addCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -27,7 +27,7 @@ class Users {
     		empty($_GET['role'])||
     		empty($_GET['status'])){
     		
-    		echo "eror";
+    		echo viewJSON "eror";
     		}
     	else{
 
@@ -48,7 +48,7 @@ class Users {
     	$id = _MainModel::table("dd_user_cards")->add(array('level' => $b, 'user_type' => $c, 'image' => $d, 'nickname' => $e, 'rating' => $f, 'description' => $g, 'role' => $h, 'status' => $j))->send();   	
     	 
     	
-    	 	_MainModel::viewJSON($id, $b, $c,$b, $d, $e, $f, $g, $h, $j);
+    	 	_MainModel::viewJSON($id);
     	 	
     	 }
 
