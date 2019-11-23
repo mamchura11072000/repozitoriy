dabb1ae81dcefe9ba325167d5a08cd08af1c1d6c
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-24 00:54:02 +0300

    editCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -27,7 +27,7 @@ class Users {
     		empty($_GET['role'])||
     		empty($_GET['status'])){
     		
-    		$eror=echo "eror";
+    		$eror=_MainModel::echo "eror";
     		}
     	else{
 
@@ -42,9 +42,7 @@ class Users {
 
     	}
     	
-    	
-    	
-    			
+    		
     	$id = _MainModel::table("dd_user_cards")->add(array('level' => $b, 'user_type' => $c, 'image' => $d, 'nickname' => $e, 'rating' => $f, 'description' => $g, 'role' => $h, 'status' => $j))->send();   	
     	 
     	
