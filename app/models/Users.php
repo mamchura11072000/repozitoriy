bce9a07cebceefb54cbf0d3ea30f7340f690e48e
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-25 11:58:37 +0300

    addCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -26,7 +26,7 @@ class Users {
     		empty($_GET['role'])||
     		empty($_GET['status'])){
     		
-    	    echo "Введены не все значения:";     		
+    	    echo "error";     		
     		}
     	else{
 
@@ -45,7 +45,7 @@ class Users {
     	$id = _MainModel::table("dd_user_cards")->add(array('level' => $b, 'user_type' => $c, 'image' => $d, 'nickname' => $e, 'rating' => $f, 'description' => $g, 'role' => $h, 'status' => $j))->send();   	
     	 
     	
-    	 	_MainModel::viewJSON($id);
+    	 	_MainModel::viewJSON($id["error" => "text errro"]);
     	 }
 
 //---------------------------------------------------------------------------------------------------------------------
