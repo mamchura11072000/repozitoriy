e5ab9aaf15585c9eb8e7c843db552b8a723b76f8
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-25 21:53:31 +0300

    deleteCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -77,11 +77,6 @@ class Users {
 //---------------------------------------------------------------------------------------------------------------------
     
     public function deleteCardsUser(){
-    	if (empty($_GET ['id'])){
-    	_MainModel::viewJSON(["error"=>"errror"]);
-    	}
-    	else{
-
     		$a = $_GET ['id'];
     		$result = _MainModel::table("dd_user_cards")->delete(array('id'=> $a))->send();
     		 _MainModel::viewJSON($result);
