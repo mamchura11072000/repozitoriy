caf71de4b512a21bfd4b3da66c25b87ffcdb64af
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-26 15:12:12 +0300

    editCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -67,7 +67,7 @@ class Users {
     		}
     	else{//_MainModel::viewJSON(["error"=>"errror"]); 
     	        	    
-    	    }$result1=_MainModel::table("dd_user_cards")->edit(array('level' =>$b, 'user_type' => $c,'image' =>$d, 'nickname'=>$e,'rating' => $f,'description' => $g,'role' => $h,'status' => $j), array('id'=>$id))->send();   	
+    	    }$result = _MainModel::table("dd_user_cards")->add(array('level' => $b, 'user_type' => $c, 'image' => $d, 'nickname' => $e, 'rating' => $f, 'description' => $g, 'role' => $h, 'status' => $j), array('id'=$id))->send();     	
     	}
 
     	
