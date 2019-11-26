5399655c3ce8f47844b06662c31b2a6a98cf8938
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-26 14:57:14 +0300

    editCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -47,19 +47,19 @@ class Users {
   
     	if (empty($_GET ['id'])){
     		$id = $_GET ['id'];
-    		 if($_GET['level']){$b = $_GET ['level'];}
-    	     if($_GET['user_type']){$c = $_GET ['user_type'];}
-    	     if($_GET['image']){$d = $_GET ['image'];}
-    	     if($_GET['nickname']){$e = $_GET ['nickname'];}
-    	     if($_GET['rating']){$f = $_GET ['rating'];}
-    	     if($_GET['description']){$g = $_GET ['description'];}
-    	     if($_GET['role']){$h = $_GET ['role'];}
-    	     if($_GET['status']){$j = $_GET ['status'];}
+    		 if(empty ($_GET['level'])){$b = $_GET ['level'];}
+    	     if(empty($_GET['user_type'])){$c = $_GET ['user_type'];}
+    	     if(empty($_GET['image'])){$d = $_GET ['image'];}
+    	     if(empty($_GET['nickname'])){$e = $_GET ['nickname'];}
+    	     if(empty($_GET['rating'])){$f = $_GET ['rating'];}
+    	     if(empty($_GET['description'])){$g = $_GET ['description'];}
+    	     if(empty($_GET['role'])){$h = $_GET ['role'];}
+    	     if(empty($_GET['status'])){$j = $_GET ['status'];}
     	        	     
     		}
     	else{//_MainModel::viewJSON(["error"=>"errror"]); 
     	        	    
-    	    }$result1=_MainModel::table("dd_user_cards")->edit(array('id'=>$id), arras('level' =>$b, 'user_type' => $c,'image' =>$d, 'nickname'=>$e,'rating' => $f,'description' => $g,'role' => $h,'status' => $j))->send();
+    	    }$result1=_MainModel::table("dd_user_cards")->edit(array('id'=>$id), array('level' =>$b, 'user_type' => $c,'image' =>$d, 'nickname'=>$e,'rating' => $f,'description' => $g,'role' => $h,'status' => $j))->send();
     	}
 
     	
