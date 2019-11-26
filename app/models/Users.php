04e8a653e8bbd8ecaa093b3cfff611b0168c7dc5
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-26 13:54:53 +0300

    editCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -47,7 +47,11 @@ class Users {
   
     	if (empty($_GET ['id'])){
     		$a = $_GET ['id'];
-    		if($_GET['level']){$b = $_GET ['level'];}
+    		}
+    	else{
+    	     _MainModel::viewJSON(["error"=>"errror"]);    	    
+    	    }
+    	     if($_GET['level']){$b = $_GET ['level'];}
     	     if($_GET['user_type']){$c = $_GET ['user_type'];}
     	     if($_GET['image']){$d = $_GET ['image'];}
     	     if($_GET['nickname']){$e = $_GET ['nickname'];}
@@ -56,10 +60,6 @@ class Users {
     	     if($_GET['role']){$h = $_GET ['role'];}
     	     if($_GET['status']){$j = $_GET ['status'];}
     	     $result1=_MainModel::table("dd_user_cards")->edit(array('level' =>$b, 'user_type' => $c, 'image' =>$d,'nickname'=>$e,'rating' => $f,'description' => $g,'role' => $h,'status' => $j), array('id'=>$a))->send();  
-    		}
-    	else{
-    	     _MainModel::viewJSON(["error"=>"errror"]);    	    
-    	    }
     	}
 
     	
