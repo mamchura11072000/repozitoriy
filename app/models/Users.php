d4372b76fc19415468be7571f965c759fb4c9b8a
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-26 12:59:10 +0300

    editCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -131,7 +131,7 @@ class Users {
 
     		
     		}
-    	  	 $idPerson = _MainModel::table("dd_user_person_data")->add(array('password' => $b, 'phone' => $c, 'phone_token' => $d, 'phone_token_data' => $e, 'doc_photo' => $f, 'surname' => $g, 'name' => $h, 'patronymic' => $j, 'timestamp' => $k, 'data_of_brith' => $l, 'address' => $m, 'coordinates' => $n, 'gender' => $o, 'other_data' => $p), array('id'=>$a))->send();
+    	  	 $idPerson = _MainModel::table("dd_user_person_data")->add(array('password' => $b, 'phone' => $c, 'phone_token' => $d, 'phone_token_data' => $e, 'doc_photo' => $f, 'surname' => $g, 'name' => $h, 'patronymic' => $j, 'timestamp' => $k, 'data_of_brith' => $l, 'address' => $m, 'coordinates' => $n, 'gender' => $o, 'other_data' => $p))->send();
     		   _MainModel::viewJSON($idPerson);
     	
     	 }
