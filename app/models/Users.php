588e18ff3c95378ce58febd3ff4f9f3ed8a2d205
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-26 15:36:31 +0300

    editCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -67,7 +67,7 @@ class Users {
     	     if(empty($_GET['role'])){$h = $_GET ['role'];}
     	     else{$j = $_GET ['status'];}
     	     if(empty($_GET['status'])){$j = $_GET ['status'];}
-    	     else{$b = $_GET ['level']}
+    	     else{$b = $_GET ['level'];}
     		    	     	    
     	    }  	$result = _MainModel::table("dd_user_cards")->edit(array('level' => $b, 'user_type' => $c, 'image' => $d, 'nickname' => $e, 'rating' => $f, 'description' => $g, 'role' => $h, 'status' => $j), array('id'=>$id))->send();   
     	          
