541a386d1a0c2cd64bff753cae60a7392fff2df2
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-27 12:18:36 +0300

    PDO1

--- app/models/_AutoInclude.php
+++ app/models/_AutoInclude.php
@@ -19,6 +19,7 @@ class _AutoInclude {
 	        		foreach ($list_dir_ch as $dir) {
 
 	        			$path = ROOT . '/app/' . $value . '/' . $dir;
+	        			var_dump($path);
 
 	        			if(is_file($path)){
 	        				  require_once $path;
