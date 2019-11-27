000bec3339043d21f6f0b958cb46ff7ff37366cd
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-27 12:00:51 +0300

    PDO1

--- app/models/Users.php
+++ app/models/Users.php
@@ -173,7 +173,7 @@ class Users {
 //--------------------------------------------------------------------------------------------------------------------
  public function getPDOUserPersonDataANDCardUser (){
  	   		
-   		$stmt = self::$db->prepare("SELECT * FROM  'dd_user_cards' WHERE id= :id");
+   		$stmt = _MainModel::$db->prepare("SELECT * FROM  'dd_user_cards' WHERE id= :id");
 
 		$result_query = $stmt->execute(array(":id" => _MainModel::$params_url['id']));
 
