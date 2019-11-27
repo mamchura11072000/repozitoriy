e212006b01cc711bb0511c737993b304e45e1b8c
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-27 16:36:07 +0300

    editCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -48,10 +48,10 @@ class Users {
     		}
     	else{
     		$id = $_GET ['id'];
-    		$arr=[];
+    		$arr=[$_GET ['level']];
     		
     		if(!empty ($_GET['level']))	{
-    		   ($b=$_GET ['level'])->$arr;
+    		   ($b=$_GET ['level']);
 
 
 
