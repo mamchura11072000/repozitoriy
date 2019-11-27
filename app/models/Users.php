ffe141f2decaff3745d3941b384e5483f1f0bce0
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-27 13:29:43 +0300

    editCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -48,14 +48,15 @@ class Users{
     		}
     	else{
     		$id = $_GET ['id'];
-    		$arr=(if(empty ($_GET['level'])){$b = $_GET ['level'];}
+    		if(empty ($_GET['level'])){$b = $_GET ['level'];}
     		if(empty($_GET['user_type'])){$c = $_GET ['user_type'];}
     	    if(empty($_GET['image'])){$d = $_GET ['image'];}
     	    if(empty($_GET['nickname'])){$e = $_GET ['nickname'];}
     	    if(empty($_GET['rating'])){$f = $_GET ['rating'];}
     	    if(empty($_GET['description'])){$g = $_GET ['description'];}
     	    if(empty($_GET['role'])){$h = $_GET ['role'];}
-    	    if(empty($_GET['status'])){$j = $_GET ['status'];});
+    	    if(empty($_GET['status'])){$j = $_GET ['status'];}
+    	    $arr=('level'=>'','user_type'=>'', 'image'=>'', 'nickname'=>'', 'rating'=>'', 'description'=>'', 'role'=>'', 'status'=>'')
     	    
 
     	    }  	$result = _MainModel::table("dd_user_cards")->edit(array($arr)), array('id'=>$id)->send();   
