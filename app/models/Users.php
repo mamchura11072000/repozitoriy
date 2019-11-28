5c9c7c937b3a5f34230fb003e5f776771e0a2813
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-28 17:17:03 +0300

    PDO1

--- app/models/Users.php
+++ app/models/Users.php
@@ -191,8 +191,8 @@ class Users extends _MainModel {
 //--------------------------------------------------------------------------------------------------------------------
  	public function getPDOAll (){
  	   		
-   		$stmt = self::$db->prepare("SELECT * FROM dd_user_cards 
-	UNION SELECT * FROM dd_user_person_data WHERE id=:id" );
+   		$stmt = self::$db->prepare("
+     SELECT dd_user_cards.* dd_user_person_data* WHERE dd_user_cards.id=dd_user_person_data.id AND id=:id;");
 
 		$result_query = $stmt->execute(array(":id" => _MainModel::$params_url ['id']));
 
