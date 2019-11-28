7256afcdd0090621e3a82e222f6eb18efd8f6293
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-28 10:15:06 +0300

    editCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -53,7 +53,7 @@ class Users extends _MainModel {
     		
     		if(!empty ($_GET['level']))	{
     			
-    		array_push($arr, $_GET['level']=>$b);
+    		array_push($arr, $b=$_GET['level']);
     		print_r($arr);
 
     		 }    		
