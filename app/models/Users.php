dc60390415e71e8f5d1722869398f4c54d990707
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-28 14:47:34 +0300

    PDO1

--- app/models/Users.php
+++ app/models/Users.php
@@ -180,7 +180,7 @@ class Users extends _MainModel {
 //--------------------------------------------------------------------------------------------------------------------
  	public function getPDOUserPersonDataANDCardUser (){
  	   		
-   		$stmt = self::$db->prepare("SELECT * FROM dd_user_cards, dd_user_person_data WHERE dd_user_cards.id = dd_user_person_data.id");
+   		$stmt = self::$db->prepare("SELECT * FROM dd_user_cards, dd_user_person_data ");
    		$result_query = $stmt->execute(array(":id" => _MainModel::$params_url ['id']));
 
 		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
