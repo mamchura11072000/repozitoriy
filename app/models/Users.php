2e21f6c669abda30ce02dd5f4f93138eeaaf4b0c
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-28 11:18:10 +0300

    editCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -51,7 +51,7 @@ class Users extends _MainModel {
     	
     		$arr=[];
     		
-    		if(!empty ($_GET['level']))	{$b=$_GET['level'];	$arr['level']=$b;print_r($arr);}
+    		if(!empty ($_GET['level']))	{$b=$_GET['level'];	$arr['level']=$b;}
     		if(!empty($_GET['user_type'])){$c = $_GET ['user_type']; $arr['user_type']=$c;}
     	    if(!empty($_GET['image'])){$d = $_GET ['image']; $arr['image']=$d;}
     	    if(!empty($_GET['nickname'])){$e = $_GET ['nickname']; $arr['nickname']=$e;}
@@ -59,6 +59,7 @@ class Users extends _MainModel {
     	    if(!empty($_GET['description'])){$g = $_GET ['description']; $arr['description']=$g;}
     	    if(!empty($_GET['role'])){$h = $_GET ['role']; $arr['role']=$h;}
     	    if(!empty($_GET['status'])){$j = $_GET ['status']; $arr['status']=$j; print_r($arr);}
+    	    print_r($arr);
             $arr = _MainModel::table("dd_user_cards")->edit(array(), array('id'=>$id))->send();
    	     
     	    } 
