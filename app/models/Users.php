7642689120df21d17fccb89144e18c651131fa1d
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-28 11:29:37 +0300

    editCardsUser

--- app/models/Users.php
+++ app/models/Users.php
@@ -59,8 +59,9 @@ class Users extends _MainModel {
     	    if(!empty($_GET['description'])){$g = $_GET ['description']; $arr['description']=$g;}
     	    if(!empty($_GET['role'])){$h = $_GET ['role']; $arr['role']=$h;}
     	    if(!empty($_GET['status'])){$j = $_GET ['status']; $arr['status']=$j;}
+    	    $arr=$result;
     	    
-    	    $result = _MainModel::table("dd_user_cards")->edit(array('level', 'user_type'), array('id'=>$id))->send();
+    	    $result = _MainModel::table("dd_user_cards")->edit(array(), array('id'=>$id))->send();
    	        } 
     	          
     	}    	
