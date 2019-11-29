a4144381851afe332002c610dc27b040ff577eb1
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-29 10:50:09 +0300

    PDO2

--- app/models/Users.php
+++ app/models/Users.php
@@ -197,7 +197,7 @@ class Users extends _MainModel {
  	   	 else{
  	   	 
  	   	 }*/	$stmt = self::$db->prepare("
-     SELECT * FROM dd_user_person_data WHERE id=:id");
+     SELECT * FROM dd_user_person_data, dd_user_cards WHERE id=:id");
 
 		$result_query = $stmt->execute(array(":id"=>_MainModel::$params_url ['id']));
 
