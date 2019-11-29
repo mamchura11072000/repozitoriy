067472c75853995c852898d96e714136ec15f456
aleksandr <57662348+mamchura11072000@users.noreply.github.com>
2019-11-29 14:01:08 +0300

    PDO2

--- app/models/Users.php
+++ app/models/Users.php
@@ -2,27 +2,16 @@
  
 class Users extends _MainModel {
 
-    public function getListUsers(){
-        $result = _MainModel::table("users_cards")->get()->send();
-    _MainModel::viewJSON($result);   }
-//---------------------------------------------------------------------------------------------------------------------
-    public function MyMetod(){
-    	$a = $_GET ['a'];
-    	$b = $_GET ['b'];
-    	$c = $_GET ['c'];
-    	$summa = $a + $b + $c;
-    	echo "Сумма $a + $b + $c = $summa";}
-         
+ 
 //---------------------------------------------------------------------------------------------------------------------    
     public function addCardsUser(){
     	
     	if (empty($_GET['level'])||empty ($_GET['user_type'])||empty($_GET['image'])||empty($_GET['nickname'])||	empty($_GET['rating'])||empty($_GET['description'])||empty($_GET['role'])||	empty($_GET['status'])){
          
-        	 _MainModel::viewJSON(["error"=>"errror"]); 	
-    		}
-    		
-    	else{
-    	    $b = $_GET ['level'];
+        	 _MainModel::viewJSON(["error"=>"errror"]); 
+
+    	}else{
+    		$b = $_GET ['level'];
     		$c = $_GET ['user_type'];
     		$d = $_GET ['image'];
     		$e = $_GET ['nickname'];
@@ -32,12 +21,12 @@ class Users extends _MainModel {
     		$j = $_GET ['status'];
 
     		$id = _MainModel::table("dd_user_cards")->add(array('level' => $b, 'user_type' => $c, 'image' => $d, 'nickname' => $e, 'rating' => $f, 'description' => $g, 'role' => $h, 'status' => $j))->send();   	
+    	     	 
     	     	 _MainModel::viewJSON($id);
     	}
-    	
     }
 
-//---------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
     
     public function editCardsUser(){
   
@@ -46,11 +35,9 @@ class Users extends _MainModel {
     	
     		 _MainModel::viewJSON(["error"=>"errror"]); 
 
-    		}
-    	else{
+    	}else{
     		$id = $_GET ['id'];
-    		;
-    	
+    		    	
     		$arr=[];
     		
     		if(!empty($_GET['level'])){$arr['level']=$_GET['level'];}
@@ -61,27 +48,23 @@ class Users extends _MainModel {
     	    if(!empty($_GET['description'])){$arr['description']=$_GET ['description'];}
     	    if(!empty($_GET['role'])){$arr['role']=$_GET ['role'];}
     	    if(!empty($_GET['status'])){$arr['status']=$_GET ['status'];}
-    	 
 
-    	   
     	    $result = _MainModel::table("dd_user_cards")->edit($arr, array('id'=>$id))->send();
-   	        } 
+   	    } 
     	          
-    	}    	
+    }    	
 
 //--------------------------------------------------------------------------------------------------------------------
     
     public function deleteCardsUser(){
     	if (empty($_GET ['id'])){
 
-			_MainModel::viewJSON(["error"=>"errror"]); 
-    		}
-    	else{
-
+			_MainModel::viewJSON(["error"=>"errror"]);
+    	}else{
     		$id = $_GET ['id'];
     		$result = _MainModel::table("dd_user_cards")->delete(array('id' => $id))->send();
-    	    }
     	}
+    }
 //--------------------------------------------------------------------------------------------------------------------
 
 	public function editStatusUser(){
@@ -89,26 +72,22 @@ class Users extends _MainModel {
     		empty($_GET['status'])){
     		
     		 _MainModel::viewJSON(["error"=>"errror"]); 
-    		}
-    	else{
+    	}else{
+    		$a = $_GET ['id'];
+    		$j = $_GET ['status'];
 
-    	$a = $_GET ['id'];
-       	$j = $_GET ['status'];
-
-        $result1=_MainModel::table("dd_user_cards")->edit(array('status' => $j), array('id'=>$a))->send();
-            }
-    	}
+    		$result1=_MainModel::table("dd_user_cards")->edit(array('status' => $j), array('id'=>$a))->send();
+        }
+    }
 
 //--------------------------------------------------------------------------------------------------------------------
 
     public function addUserPersonData(){
-    	if (empty($_GET['password'])||empty ($_GET['phone'])||empty($_GET['phone_token'])||empty($_GET['phone_token_data'])||empty($_GET['doc_photo'])||empty($_GET['surname'])||empty($_GET['name'])||empty($_GET['patronymic'])||empty($_GET['timestamp'])||empty($_GET['data_of_brith'])||empty($_GET['address'])||empty($_GET['coordinates'])||empty($_GET['gender'])||empty($_GET['other_data']))	{
+    	if (empty($_GET['password'])||empty ($_GET['phone'])||empty($_GET['phone_token'])||empty($_GET['phone_token_data'])||empty($_GET['doc_photo'])||empty($_GET['surname'])||empty($_GET['name'])||empty($_GET['patronymic'])||empty($_GET['timestamp'])||empty($_GET['data_of_brith'])||empty($_GET['address'])||empty($_GET['coordinates'])||empty($_GET['gender'])||empty($_GET['other_data'])){
 
     		_MainModel::viewJSON(["error"=>"errrror"]);
     	    	      		
-    		}
-    	else{    
-
+        }else{ 
     	 	$b = $_GET ['password'];
     		$c = $_GET ['phone'];
     		$d = $_GET ['phone_token'];
@@ -124,13 +103,12 @@ class Users extends _MainModel {
     		$o = $_GET ['gender'];
     		$p = $_GET ['other_data'];	
 
-    		
-    		}
-    	  	 $id = _MainModel::table("dd_user_person_data")->add(array('password' => $b, 'phone' => $c, 'phone_token' => $d, 'phone_token_data' => $e, 'doc_photo' => $f, 'surname' => $g, 'name' => $h, 'patronymic' => $j, 'timestamp' => $k, 'data_of_brith' => $l, 'address' => $m, 'coordinates' => $n, 'gender' => $o, 'other_data' => $p))->send();
+    		$id = _MainModel::table("dd_user_person_data")->add(array('password' => $b, 'phone' => $c, 'phone_token' => $d, 'phone_token_data' => $e, 'doc_photo' => $f, 'surname' => $g, 'name' => $h, 'patronymic' => $j, 'timestamp' => $k, 'data_of_brith' => $l, 'address' => $m, 'coordinates' => $n, 'gender' => $o, 'other_data' => $p))->send();
     		   _MainModel::viewJSON($id);
-    	
-    	 }
-
+    	}
+    	  	 
+    }	
+   
 
 //--------------------------------------------------------------------------------------------------------------------
 
@@ -139,12 +117,11 @@ class Users extends _MainModel {
 
     		_MainModel::viewJSON(["error"=>"errrror"]);
     	    	      		
-    		}
-    	else{    
-
-    	 	$a = $_GET ['id'];
+    	}else{
+    		$a = $_GET ['id'];
 
     	 	$arr=[];
+
     		if(!empty($_GET['password'])){$arr['password']=$_GET ['password'];}
     		if(!empty($_GET['phone'])){$arr['phone']=$_GET ['phone'];}
     		if(!empty($_GET['phone_token'])){$arr['phone_token']=$_GET ['phone_token'];}
@@ -159,9 +136,10 @@ class Users extends _MainModel {
     		if(!empty($_GET['coordinates'])){$arr['coordinates']=$_GET ['coordinates'];}
     		if(!empty($_GET['gender'])){$arr['gender']=$_GET ['gender'];}
     		if(!empty($_GET['other_data'])){$arr['other_data']=$_GET ['other_data'];}
+    		
     		$result = _MainModel::table("dd_user_person_data")->edit($arr, array('id'=>$a))->send();    		
-    	        }
-    	    }
+    	}
+    }
 
 
 //--------------------------------------------------------------------------------------------------------------------
@@ -170,43 +148,38 @@ class Users extends _MainModel {
     	if (empty($_GET ['id'])){
 
 			_MainModel::viewJSON(["error"=>"errror"]); 
-    		}
-    	else{
+    	}else{
 
     		$id = $_GET ['id'];
     		$result = _MainModel::table("dd_user_person_data")->delete(array('id' => $id))->send();
-    	    }
     	}
+    }
 
 //--------------------------------------------------------------------------------------------------------------------
  	public function getPDOUserPersonDataANDCardUser (){
- 	   		
-   		$stmt = self::$db->prepare("SELECT * FROM dd_user_cards, dd_user_person_data ");
-   		$result_query = $stmt->execute(array());
+ 		if(empty(_MainModel::$params_url ['id'])){
 
-		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+ 			_MainModel::viewJSON(["Error"=>"отсутствует id"]);
+ 	   }else{
 
-		_MainModel::viewJSON($rows);
-	}
+ 	   		$stmt = self::$db->prepare("SELECT * FROM dd_user_person_data JOIN dd_user_cards WHERE dd_user_cards.id = dd_user_person_data.id AND dd_user_person_data.id=:id");
+ 	   		$result_query = $stmt->execute(array(":id"=>_MainModel::$params_url ['id']));
+ 	   		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+ 	   		_MainModel::viewJSON($rows);
 
-//--------------------------------------------------------------------------------------------------------------------
- 	public function getPDOAll (){
- 	   	if(empty(_MainModel::$params_url ['id'])){
-
- 	   	_MainModel::viewJSON(["Error"=>"отсутствует id"]);
- 	   	    }
- 	   	 else{
- 	   	 	$stmt = self::$db->prepare("SELECT * FROM dd_user_person_data JOIN dd_user_cards WHERE dd_user_cards.id = dd_user_person_data.id AND dd_user_person_data.id=:id");
-
-		$result_query = $stmt->execute(array(":id"=>_MainModel::$params_url ['id']));
-
-		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
-
-		_MainModel::viewJSON($rows);
-
- 	   	 }	
+ 	   	}
+ 	}	
  	   
-   		 	  
+   		
+//--------------------------------------------------------------------------------------------------------------------
+ 	  
+	public function getPDOAll (){
+
+			$stmt = self::$db->prepare("SELECT * FROM dd_user_cards, dd_user_person_data ");
+   			$result_query = $stmt->execute(array());
+			$rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+
+			_MainModel::viewJSON($rows);
 	}
 
 }
